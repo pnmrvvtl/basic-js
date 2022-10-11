6002a6eb6438843d98b02e5155ee496b2364dadb
pnmrvvtl <pnmrv.vtl@gmail.com>
2022-10-11 18:46:28 +0300

    feat: create solution for first three tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
@@ -6,22 +6,35 @@ const HALF_LIFE_PERIOD = 5730;
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+
+    let result = false;
+    if (typeof sampleActivity != 'string' ||
+        isNaN(Number(sampleActivity)) ||
+        Number(sampleActivity) <= 0 ||
+        Number(sampleActivity) > 15) {
+        return result;
+    }
+
+    const MODERN_ACTIVITY = 15;
+    const HALF_LIFE_PERIOD = 5730;
+
+    result = Math.ceil((Math.log(MODERN_ACTIVITY / Number(sampleActivity))) / (0.693 / HALF_LIFE_PERIOD));
+
+    return result;
 }
 
 module.exports = {
-  dateSample
+    dateSample
 };
